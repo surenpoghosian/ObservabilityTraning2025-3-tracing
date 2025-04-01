ce41db175906e83d5c6421b6204b9e07d254b9e4
Suren Poghosyan <suren.poghosian.18@gmail.com>
2025-04-01 15:08:36 +0400

    [logs-loki] add loki to visualize logs in grafana

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,29 @@
-import pino from "pino";
+import pino from 'pino';
 
 const logger = pino({
   transport: {
-    target: "pino-pretty",
+    target: 'pino-loki',
     options: {
-      colorize: true,
-      translateTime: "HH:MM:ss",
-      ignore: "pid,hostname"
+      host: 'http://loki:3100',
+      labels: { service: 'node-app', env: 'dev' },
     }
-  },
+  }
 });
 
 export default logger;
+
+
+// import pino from "pino";
+
+// const logger = pino({
+//   transport: {
+//     target: "pino-pretty",
+//     options: {
+//       colorize: true,
+//       translateTime: "HH:MM:ss",
+//       ignore: "pid,hostname"
+//     }
+//   },
+// });
+
+// export default logger;
